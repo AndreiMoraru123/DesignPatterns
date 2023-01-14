9c7299f0ce9bd1ad90642edd927add9760549fb6
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-01-14 15:51:43 +0200

    Add Tests & Ref Count

--- SmartPointer.h
+++ SmartPointer.h
@@ -7,22 +7,43 @@
 
 #include <typeinfo>
 #include <utility>
+#include<cstddef>
 
 template <class T>
 class SmartPtr {
     T *ptr;
+    int *ref_count;
 public:
     // Constructors
     // The default constructor initializes the pointer with nullptr
     // Example: SmartPtr<int> ptr;
-    explicit SmartPtr(T *p = nullptr) { ptr = p; }
+    explicit SmartPtr(T *p = nullptr) : ptr(p), ref_count(new int(1)) {}
+
     // The move constructor takes the ownership of the pointer from the rvalue reference
+    // It is used to move the value of a temporary object of type T into a SmartPtr
     // Example: SmartPtr<int> ptr1 = SmartPtr<int>(new int(5));
-    explicit SmartPtr(T&& other) : ptr(new T(std::move(other))) {}
+    explicit SmartPtr(T&& other) : ptr(new T(std::move(other))), ref_count(new int(1)) {}
+
+    // This second move constructor is a move constructor for the smart pointer class itself
+    // It is used to move the ownership of the pointer from one smart pointer to another
+    // Example: SmartPtr<int> ptr1 = SmartPtr<int>(new int(5));
+    //          SmartPtr<int> ptr2 = std::move(ptr1);
+    SmartPtr(SmartPtr<T>&& other) noexcept : ptr(other.ptr), ref_count(other.ref_count) {
+        other.ptr = nullptr;
+        other.ref_count = nullptr;
+    }
 
     // Destructor
     // This is the bread and butter of the smart pointer
-    ~SmartPtr() { delete(ptr); }
+//    ~SmartPtr() { delete(ptr); }
+
+    ~SmartPtr() {
+        --(*ref_count);
+        if(*ref_count == 0) {
+            delete ptr;
+            delete ref_count;
+        }
+    }
 
     // Overloading de-referencing operator
     T &operator*() const { return *ptr; }
@@ -37,6 +58,8 @@ public:
     // SmartPtr<int> ptr2(ptr1);
     SmartPtr(const SmartPtr<T> &other) {
         ptr = new T(*other.ptr);
+        ref_count = other.ref_count;
+        ++(*ref_count);
     }
 
     // Copy assignment operator
@@ -44,10 +67,17 @@ public:
     // Example: SmartPtr<int> ptr1 = new int(10);
     // SmartPtr<int> ptr2 = new int(20);
     // ptr2 = ptr1;
+    // Copy assignment operator
     SmartPtr<T> &operator = (const SmartPtr<T> &other) {
         if (this != &other) {
-            delete ptr;
+            --(*ref_count);
+            if (*ref_count == 0) {
+                delete ptr;
+                delete ref_count;
+            }
             ptr = new T(*other.ptr);
+            ref_count = other.ref_count;
+            ++(*ref_count);
         }
         return *this;
     }
@@ -59,12 +89,20 @@ public:
     // ptr2 = std::move(ptr1);
     SmartPtr<T> &operator = (SmartPtr<T>&& other)  noexcept {
         if (this != &other) {
-            delete ptr;
+            --(*ref_count);
+            if (*ref_count == 0) {
+                delete ptr;
+                delete ref_count;
+            }
             ptr = other.ptr;
+            ref_count = other.ref_count;
             other.ptr = nullptr;
+            other.ref_count = new int(1);
+            ++(*ref_count);
         }
         return *this;
     }
+
 
     // Dynamic cast
     // Used to cast a SmartPtr to a different type
@@ -98,6 +136,15 @@ public:
     // int* ptr2 = ptr1.get();
     T* get() const {
         return ptr;
+    }
+
+    // Use count
+    // Used to get the number of SmartPtrs that point to the same object
+    // Example: SmartPtr<int> ptr1 = new int(10);
+    // SmartPtr<int> ptr2 = ptr1;
+    // int count = ptr1.use_count();
+    [[nodiscard]] int use_count() const {
+        return ptr == nullptr ? 0 : *ref_count;
     }
 };
 
---
+++ TestPointer.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Andrei on 14-Jan-23.
+//
+
+#include "SmartPointer.h"
+#include <cassert>
+
+class Test {
+public:
+    static void TestDefaultConstructor() {
+        SmartPtr<int> ptr;
+        assert(ptr.get() == nullptr);
+        assert(ptr.use_count() == 0);
+    }
+
+    static void TestMoveConstructor() {
+        SmartPtr<int> ptr1 = SmartPtr<int>(new int(10));
+        SmartPtr<int> ptr2 = std::move(ptr1);
+        assert(ptr1.get() == nullptr);
+        assert(ptr2.get() != nullptr);
+        assert(ptr2.use_count() == 1);
+    }
+
+    static void TestCopyConstructor() {
+        SmartPtr<int> ptr1 = SmartPtr<int>(new int(10));
+        SmartPtr<int> ptr2 = ptr1;
+        assert(ptr1.get() != nullptr);
+        assert(ptr2.get() != nullptr);
+        assert(ptr1.use_count() == 2);
+        assert(ptr2.use_count() == 2);
+    }
+};
+
+int main() {
+    Test::TestDefaultConstructor();
+//    Test::TestMoveConstructor();
+    Test::TestCopyConstructor();
+    return 0;
+}
