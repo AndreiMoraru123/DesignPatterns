1f6fd7a4f3c7f041c65cce831f9abfb465cefc60
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-01-30 11:57:22 +0200

    TemplateMethod DP

---
+++ Behavioral/TemplateMethod.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <string>
+#include "../SmartPointer.h"
+
+/*
+ * The Abstract Class defines a template method that contains th skeleton of
+ * an algorithm, composed of calls to (usually) abstract primitive operations.
+ *
+ * Concrete subclasses should implement these operations, but leave the template
+ * method itself intact.
+ */
+
+class AbstractClass {
+    /*
+     * The template method defines the skeleton of an algorithm.
+     */
+    public:
+        void TemplateMethod() const {
+            this->BaseOperation1();
+            this->RequiredOperations1();
+            this->BaseOperation2();
+            this->Hook1();
+            this->RequiredOperation2();
+            this->BaseOperation3();
+            this->Hook2();
+        }
+
+        /*
+         * These operations already have implementations.
+         */
+
+        void BaseOperation1() const {
+            std::cout << "AbstractClass says: I am doing the bulk of the work\n";
+        }
+
+        void BaseOperation2() const {
+            std::cout << "AbstractClass says: But I let subclasses override some operations\n";
+        }
+
+        void BaseOperation3() const {
+            std::cout << "AbstractClass says: But I am doing the bulk of the work anyway\n";
+        }
+
+        /*
+         * These operations have to be implemented in subclasses.
+         */
+
+        virtual void RequiredOperations1() const = 0;
+        virtual void RequiredOperation2() const = 0;
+
+        /*
+         * These are "hooks." Subclasses may override them, but it's not mandatory
+         * since the hooks already have default (but empty) implementation. Hooks
+         * provide additional extension points in some crucial places of the algorithm.
+         */
+
+        virtual void Hook1() const {}
+        virtual void Hook2() const {}
+};
+
+/*
+ * Concrete classes have to implement all abstract operations of the base class.
+ * They can also override some operations with a default implementation.
+ */
+
+class ConcreteClass1 : public AbstractClass {
+    public:
+        void RequiredOperations1() const override {
+            std::cout << "ConcreteClass1 says: Implemented Operation1\n";
+        }
+
+        void RequiredOperation2() const override {
+            std::cout << "ConcreteClass1 says: Implemented Operation2\n";
+        }
+};
+
+/*
+ * Usually, concrete classes override only a fraction of base class' operations.
+ */
+
+class ConcreteClass2 : public AbstractClass {
+    public:
+        void RequiredOperations1() const override {
+            std::cout << "ConcreteClass2 says: Implemented Operation1\n";
+        }
+
+        void RequiredOperation2() const override {
+            std::cout << "ConcreteClass2 says: Implemented Operation2\n";
+        }
+
+        void Hook1() const override {
+            std::cout << "ConcreteClass2 says: Overridden Hook1\n";
+        }
+};
+
+/*
+ * The client code calls the template method to execute the algorithm. Client
+ * code does not have to know the concrete class of an object it works with, as
+ * long as it works with objects through the interface of their base class.
+ */
+
+void ClientCode(const SmartPtr<AbstractClass>& class_) {
+    // ...
+    class_->TemplateMethod();
+    // ...
+}
+
+int main() {
+    std::cout << "Same client code can work with different subclasses:\n\n";
+    SmartPtr<AbstractClass> concrete_class1 = SmartPtr<AbstractClass>(new ConcreteClass1);
+    ClientCode(concrete_class1);
+    std::cout << "\n";
+    std::cout << "Same client code can work with different subclasses:\n";
+    SmartPtr<AbstractClass> concrete_class2 = SmartPtr<AbstractClass>(new ConcreteClass2);
+    ClientCode(concrete_class2);
+}
